3427c56ab9ce720dac06ec6925af4cdb0a812229
Sara Tasche <sara.dolores.tasche@gmail.com>
2025-07-02 22:23:38 +0200

    Some rewrite.
    Signed-off-by: Sara Tasche <sara.dolores.tasche@gmail.com>

--- minimal/error/errors.d
+++ minimal/error/errors.d
@@ -1,77 +1,188 @@
 module minimal.error.errors;
 
-import minimal.error.type : IError, ErrorLevel, TError, TContext;
+import std.container.array : Array;
+import std.path : isValidPath, asRelativePath, asAbsolutePath;
 
-const string LOGMESSAGE_FMT              = "%s; at %s:%s in `%s`";
-const string ERRMESSAGE_FMT_NONCATCHABLE = "%s  ([%s] %s)";
-const string ERRMESSAGE_FMT_CATCHABLE    = "%s  ([%s] %s)";
+enum ErrorLevel {
+    OK,
+    Info,
+    Warning,
+    Error,
+    Critical,
+}
 
-
-abstract class ErrorClass (string Name, ErrorLevel Level) : IError
+interface IError
 {
-    protected:
-    TError _error;
+    IError previous();
+    bool   catchable();
+
+    void raise();
+    bool trycatch();
+    bool trycatch(bool delegate(IError) dg);
+    void nocatch();
+    bool ok();
+
+    uint   code();
+    string message();
+    string messagef(string fmt);
+
+    T opCast(T: bool)    () const;
+    T opCast(T: Context) () const;
+    T opCast(T: uint)    () const;
+}
+
+alias ErrorList   = Array!BaseError;
+alias ContextList = Array!Context;
+
+ErrorList list = ErrorList();
+
+struct Context
+{
+    ulong     line     = 0;
+    ulong     column   = 0;
+    ulong     position = 0;
+    string    file     = "{NONE}";
+    BaseError error;
+}
+
+final class NoError : IError
+{
+    static import std.stdio;
+
+    this() {};
+
+    immutable(IError) previous()            => this;
+    bool catchable()                        => true;
+    bool ok()                               => true;
+    bool trycatch()                         => true;
+    bool trycatch(bool delegate(IError) dg) => true;
+
+    void raise()   { debug writeln("{ NoError.raise   }"); }
+    void nocatch() { debug writeln("{ NoError.nocatch }"); }
+
+    T opCast(T: bool)      () const => this.catchable;
+    T opCast(T: Context)   () const => this._context_;
+    T opCast(T: uint)      () const => this._code_;
+    T opCast(T: BaseError) () const => new BaseError(ErrorLevel.OK, "No error", 0, 0, 0, 0);
+}
+
+final class BaseError : IError
+{
+    public import std.logger : FileLogger;
+
+    private:
+    FileLogger _logger_   = new FileLogger("Errors.log");
+    IError     _previous_ = new NoError();
+    Context    _context_;
+
+    ErrorLevel _level_     = ErrorLevel.OK;
+    string     _message_   = "OK";
+    uint       _code_      = 0;
+    bool       _catchable_ = false;
+    bool       _resolved_  = false;
 
     public:
-    @disable this();
-
-    this(string message)
-    in (message.length > 0) {
-        this._error = TError(Level, message, 1, TContext(0, 0, "<none>", true));
+    this(ErrorLevel lv, string message, uint code, ulong line, ulong column, ulong position = 0, string file = "{NONE}", IError prev = new NoError()) {
+        this.setup(lv, message, code, line, column, position, file);
+        this._previous_ = prev;
+        .list.insertBack(this);
     }
 
-    this(string message, uint code)
-    in (code > 0, message.length > 0) {
-        this._error = TError(Level, message, code, TContext(0, 0, "<none>", true));
+    this(string message, uint code, ulong line, ulong column, ulong position = 0, string file = "{NONE}", IError prev = new NoError()) {
+        ErrorLevel lv;
+
+        switch (code) {
+            case 0:
+                lv = ErrorLevel.OK;
+            break;
+            case 1, 2:
+                lv = ErrorLevel.Info;
+            break;
+            case 3, 4, 5:
+                lv = ErrorLevel.Warning;
+            break;
+            case 6, 7, 8, 9, 10:
+                lv = ErrorLevel.Error;
+            break;
+            default:
+                lv = ErrorLevel.Critical;
+        }
+
+        this.setup(lv, message, code, line, column, position, file);
+        this._previous_ = prev;
+        .list.insertBack(this);
     }
 
-    this(string message, uint code, ulong line, ulong column, string file = "<none>", bool catchable = true) {
-        this._error = TError(this.Level, message, code, TContext(line, column, file, catchable));
+    void replaceLogfile(string file)
+    in (isValidPath(asRelative(file))) {
+        string path   = buildNormalizedPath(getcwd, asRelative(file));
+        this._logger_ = new shared FileLogger(path);
     }
 
-    string     name()  => this.Name;
-    ErrorLevel level() => this.Level;
-
-    bool catchable() {
-        return this._error.context.catchable;
+    void log() {
+        switch (this._level_) {
+            case ErrorLevel.OK:       break;
+            case ErrorLevel.Info:     this._logger_.info(this.message);     break;
+            case ErrorLevel.Warning:  this._logger_.warning(this.message);  break;
+            case ErrorLevel.Error:    this._logger_.error(this.message);    break;
+            case ErrorLevel.Critical: this._logger_.critical(this.message); break;
+            default:                  this._logger_.log(this._message_);
+        }
     }
 
-    string messagef(string fmt) {
+    immutable(FileLogger) logger()   => cast(immutable) this._logger_;
+    immutable(IError)     previous() => cast(immutable) this._previous_;
+
+    bool catchable()
+    in (this._level_ != ErrorLevel.OK) {
+        return (this._level_ != ErrorLevel.Critical);
+    }
+
+    void raise()
+    in (this._level_ != ErrorLevel.OK) {
+        if(!catchable) this.nocatch();
+    }
+
+    bool trycatch()
+    in (this._level_ != ErrorLevel.OK) {
+        if(this.catchable) return true;
+        this.raise();
+        return false;
+    }
+
+    bool trycatch(bool delegate(IError) dg) {
+        if(this.catchable) return true;
+        return dg(this);
+    }
+
+    void nocatch()
+    in (this._level_ != ErrorLevel.OK) {
         import std.format : format;
-        return fmt.format(this._code, this._message);
+        string message = "[%s]: %s".format(this._code_, this._message_);
+        throw new Exception(message);
     }
+
+    bool ok() => (this._level_ == ErrorLevel.OK);
 
     string message() {
         return this.messagef("[%s] %s");
     }
 
-    uint code() {
-        return this._error._code;
+    string messagef(string fmt) {
+        return fmt.format(cast(string) this.code, this.message);
     }
 
-    bool resolved() {
-        return this._error.resolved;
-    }
+    uint    code    () const => this._code_;
+    Context context () const => this._context_;
 
-    bool trycatch(bool delegate(TError) dg) {
-        if (this.catchable && dg(this._error)) {
-            import std.logger : warningf;
-            warningf(ERRMESSAGE_FMT_CATCHABLE, "Catched", this.code, this.message);
-            return true;
-        }
-        else {
-            import std.logger : criticalf;
-            criticalf(ERRMESSAGE_FMT_NONCATCHABLE, "Non-Catchable", this.code, this.message);
-            return false;
-        }
-    }
+    T opCast(T: bool)    () const => this.catchable;
+    T opCast(T: Context) () const => this._context_;
+    T opCast(T: uint)    () const => this._code_;
 
-    void nocatch() {
-        import std.logger : criticalf;
-        criticalf(ERRMESSAGE_FMT_NONCATCHABLE, "Non-Catchable", this.code, this.message);
+    private void setup(ErrorLevel lv, string message, uint code, ulong line, ulong column, ulong position = 0, string file = "{NONE}") pure {
+        this._level_   = lv;
+        this._message_ = message;
+        this._code_    = code;
+        this._context_ = Context(line, column, position, file, this);
     }
-
-    T opCast(T: bool)     () const { return this.catchable;      }
-    T opCast(T: TError)   () const { return this._error;         }
-    T opCast(T: TContext) () const { return this._error.context; }
 }
--- minimal/error/predefined.d
+++ minimal/error/predefined.d
@@ -1,35 +1,13 @@
 module minimal.error.predefined;
 
-import minimal.error.type    : IError, ErrorLevel;
-import minimal.error.errors  : ErrorClass;
+import minimal.error.errors;
 
-final class InputError  : IError, ErrorClass!("Input",  ErrorLevel.Error) {}
-final class SyntaxError : IError, ErrorClass!("Syntax", ErrorLevel.Error) {}
+final class InputError  : BaseError
+{
+    override bool catchable() => true;
+}
 
-
-void log(in TContext context) {
-    import std.logger : infof, warningf, errorf, criticalf;
-    debug import std.stdio : writefln;
-    import std.typecons : tuple, Tuple;
-
-    Tuple c = tuple(context.line, context.column, context.file);
-
-    switch(this.level) {
-        case ErrorLevel.OK:
-            debug writeln("OK \"%s\" >", this.messagef);
-        break;
-        case ErrorLevel.Info:
-            infof(   "%s; at %s:%s in `%s`", this.messagef, c.expand);
-        break;
-        case ErrorLevel.Warning:
-            warningf("%s; at %s:%s in `%s`", this.messagef, c.expand);
-        break;
-        case ErrorLevel.Error:
-            errorf(  "%s; at %s:%s in `%s`", this.messagef, c.expand);
-        break;
-        case ErrorLevel.Critical:
-        goto default;
-        default:
-            criticalf(ERRMESSAGE_FMT_NONCATCHABLE[0], ERRMESSAGE_FMT_NONCATCHABLE[1], this.code, this.message);
-    }
+final class SyntaxError : BaseError
+{
+    override bool catchable() => false;
 }
